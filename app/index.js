cac45520cb2ea34453d743bfea6335077aba7a0d
Julie Valet <julie.valet@gmail.com>
2017-10-17 19:04:37 -0400

    Responsive menu added

--- app/index.js
+++ app/index.js
@@ -13,3 +13,24 @@ fetch(resumeJSONPath)
         // console.log(data);
         ReactDOM.render(<App resumeData={data} />, document.getElementById('app'));
     });
+
+
+const menu = document.querySelector('#menu');
+const anchorMenu = document.querySelector('#menu-toggle');
+
+/* Functions */
+function toggleMenu() {
+    if (menu.classList.contains('open')) {
+        menu.classList.remove('open');
+    } else {
+        menu.classList.add('open');
+    }
+
+    // Calculate where the botton of anchorMenu is located
+    const anchorBottom = anchorMenu.offsetHeight + anchorMenu.offsetTop + 3;
+    // Position the top of the dropdown menu
+    menu.style.top = `${anchorBottom}px`;
+}
+
+/* Event listeners */
+anchorMenu.addEventListener('click', toggleMenu);
