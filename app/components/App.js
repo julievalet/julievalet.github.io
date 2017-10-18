1901c5ee857be318bb6f19ae87ad8927601302fd
Julie Valet <julie.valet@gmail.com>
2017-10-18 19:36:18 -0400

    Bettered component to display dev skill (star rating added)

--- app/components/App.js
+++ app/components/App.js
@@ -3,18 +3,25 @@ import { ListSkillDev } from './ListSkillDev';
 
 export class App extends React.Component {
     render() {
-        const resumeData = this.props.resumeData;
-        const listSkill = resumeData.skills.dev.frontEnd
-            .map( (skillType, index) => {
-                console.log(skillType);
-                return <ListSkillDev item={skillType} key={index} />;
-            });
+        const resumeData = this.props.resumeData; // All the data of the resume
 
-        console.log(listSkill);
+        const skillTypes = resumeData.skills.dev; // Dev skills
+        let listSkills = [];
+        Object.keys(skillTypes).forEach(key => {
+            const skillType = <h3 key={key}>{key}</h3>;
+            listSkills.push(skillType);
+            listSkills = listSkills.concat( // Concat all skills
+                skillTypes[key].map( (skill, index) => {
+                    return <ListSkillDev item={skill} key={skill.name} />;
+                })
+            );
+        });
+            
+        // console.log(listSkills);
         return (
             <div>
                 <h1>{ resumeData.info.title }</h1>
-                <div>{ listSkill }</div>
+                <div>{ listSkills }</div>
             </div>
         );
     }
--- app/components/ListSkillDev.js
+++ app/components/ListSkillDev.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
+class SkillLevel extends React.Component {
+    render() {
+        const level = this.props.level;
+        const starMap = level.map( (star, index) => {
+            return star===1 ? <i key={'star-'+index} className="material-icons">star</i> : <i  key={'star-'+index} className="material-icons">star_border</i>;
+        });
+        return (
+            <li>
+               { starMap } 
+            </li>
+        );
+    }
+}
 export class ListSkillDev extends React.Component {
     render() {
         const item = this.props.item;
         return (
             <ul>
                 <li>{ item.name }</li>
+                <SkillLevel level={item.level}/>
                 <li>{ item.time }</li>
             </ul>
         );
     }
 }
+
