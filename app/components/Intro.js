835593816693d05bf697ee0fcd4d8ed84f30eddf
Julie Valet <julie.valet@gmail.com>
2017-10-25 15:07:50 -0400

    Worked on Intro responsiveness

--- app/components/Intro.js
+++ app/components/Intro.js
@@ -4,8 +4,14 @@ export class Intro extends React.Component {
     render() {
         return (
             <div id="intro">
-                <h2>{ this.props.info.title }</h2>
-                {<img src={ `app/${this.props.info.pathPicture}` } alt="pic"/>}
+                <div>
+                    <h2>{ this.props.info.title }</h2>
+                    <div className="pic"><img src={ `app/${this.props.info.pathPicture}` } alt="pic"/></div>
+                </div>
+                <div>
+                    { this.props.info.location }<br/>
+                    Available : { this.props.info.available }
+                </div>
            </div>
         )
     }
