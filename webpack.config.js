a2faee4b00617700b418fce93945f8407e6c7295
Julie Valet <julie.valet@gmail.com>
2018-01-04 18:48:23 -0500

    Change dir name to src

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,34 @@
 var HTMLWebpackPlugin = require('html-webpack-plugin');
 var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
-    template: __dirname + '/app/index.html',
+    template: __dirname + '/src/index.html',
     filename: 'index.html',
     inject: 'body'
 });
 
 const webpack = require('webpack');
-const HMRPlugin = new webpack.HotModuleReplacementPlugin()
+const HMRPlugin = new webpack.HotModuleReplacementPlugin();
 const nodeEnv = process.env.NODE_ENV || 'production';
 
 module.exports = {
-  devtool: 'source-map',
-  entry: [
-    __dirname + '/app/index.js',
-    'webpack-dev-server/client?http://localhost:8080/'
-  ],
-  output: {
-    filename: 'bundle.js',
-    path: __dirname + '/build'
-  },
-  devServer: {
-    historyApiFallback: true
-    // hot: true,
-    // inline: true
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js[x]?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      }
-    ]
-  },
-  plugins: [HTMLWebpackPluginConfig/*, HMRPlugin*/]
+    devtool: 'source-map',
+    entry: [__dirname + '/src/index.js', 'webpack-dev-server/client?http://localhost:8080/'],
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/build'
+    },
+    devServer: {
+        historyApiFallback: true
+        // hot: true,
+        // inline: true
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js[x]?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [HTMLWebpackPluginConfig /*, HMRPlugin*/]
 };
